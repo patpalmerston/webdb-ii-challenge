07138893fc7d25de5f0bf43d3021a02f752deefb
patpalmerston <patpalmerston@gmail.com>
2019-05-07 15:31:15 -0700

    stretch

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const configureMiddleware = require('../config/middleware');
 
 const zoosRouter = require('../routers/zoos-router.js');
+const bearsRouter = require('../routers/bears-router.js')
 
 const server = express();
 
@@ -9,5 +10,6 @@ configureMiddleware(server);
 
 
 server.use('/api/zoos', zoosRouter);
+server.use('/api/bears', bearsRouter)
 
 module.exports = server;
